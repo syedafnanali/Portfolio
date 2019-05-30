0060abee563eb50281bbc3e9ac595a56e837337d
syedafnanali <syedafnanali@hotmail.com>
2019-05-30 16:06:57 -0500

    portfolio page

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-var switchStatus = false;
-$("#navi-toggle").on("change", function() {
-  if ($(this).is(":checked")) {
-    switchStatus = $(this).is(":checked");
-    // alert(switchStatus); // To verify
+// var switchStatus = false;
+// $("#navi-toggle").on("change", function() {
+//   if ($(this).is(":checked")) {
+//     switchStatus = $(this).is(":checked");
+//     // alert(switchStatus); // To verify
 
-  } else {
-    switchStatus = $(this).is(":checked");
-    // alert(switchStatus); // To verify
-  }
+//   } else {
+//     switchStatus = $(this).is(":checked");
+//     // alert(switchStatus); // To verify
+//   }
+// });
+
+$('#navLink-about').click((e) => {
+ e.preventDefault();
+ $('#navi-toggle').prop('checked',false);
+ $('html,body').animate({
+   scrollTop: $('#about').offset().top
+ }, 200);
 });
+
 
 
